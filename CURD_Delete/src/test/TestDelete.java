c6701401ff1ef6fb300563e0c8a8ac4c78e098e3
RAMESH RANJAN <RAMESH RANJAN@RAMESH>
2018-04-30 23:39:59 +0530

    CRUD -Delete

---
+++ CURD_Delete/src/test/TestDelete.java
@@ -0,0 +1,32 @@
+package test;
+
+import org.apache.log4j.BasicConfigurator;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+import beans.Student;
+
+public class TestDelete {
+
+	public static void main(String[] args) {
+		BasicConfigurator.configure();
+		Configuration cfg=new Configuration();
+		cfg.configure("resources/hibernate.cfg.xml");
+		SessionFactory sf=cfg.buildSessionFactory();
+		Session session=sf.openSession();
+		Transaction t=session.beginTransaction();
+		
+		Student st= new Student();
+		st.setId(222);
+		
+		session.delete(st);
+		
+		t.commit();
+		session.close();
+		sf.close();
+		System.out.println("Delete Successful");
+	}
+
+}
